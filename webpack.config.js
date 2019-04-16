ff911330926b4b86b979072d7bb3237251312d77
silverbulletlee <silverbulletlee@gmail.com>
2019-04-17 02:11:25 +0800

    [chore] 增加本地调试 & 添加npm镜像配置

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+/**
+ * @file webpack config
+ * @author silverbulletlee
+ * @Date 2019.04.17
+ */
+
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const address = require('address');
+const opn = require('opn');
+
+const ipv4 = address.ip() || '127.0.0.1';
+const port = 3000;
+
+module.exports = {
+    entry: './test/index.js',
+    output: {
+        path: path.resolve(__dirname, 'output'),
+        filename: 'index_[hash:8].js'
+    },
+    mode: 'development',
+    plugins: [
+        new HtmlWebpackPlugin({
+            inject: 'body',
+            title: '数据结构与算法TypeScript实现'
+        })
+    ],
+    devServer: {
+        contentBase: path.join(__dirname, "output"),
+        port,
+        host: '0.0.0.0',
+        after: () => {
+            opn(`http://${ipv4}:${port}`);
+        }
+    }
+};
