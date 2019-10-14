7d1c0f4679ef448324ec97652b0de18e7ba63c6b
silverbulletlee <silverbulletlee@gmail.com>
2019-10-15 00:04:31 +0800

    [chore] 添加自动tag脚本

---
+++ build/release.js
@@ -0,0 +1,12 @@
+/**
+ * @file: 写一个简单的脚本，根据package.json version 自动打tag，推送npm
+ * @author: silverbulletlee
+ */
+
+const { execSync } = require('child_process');
+
+const version = require('../package').version;
+
+const getTags = () => execSync('git tag').toString().split('\n');
+
+!getTags().includes(version) && execSync(`git tag v${version}`);
