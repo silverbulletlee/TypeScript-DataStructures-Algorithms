b54d222aaa38b1ab3737cedc8d9d9f7e5280e2c7
silverbulletlee <silverbulletlee@gmail.com>
2019-10-15 00:24:35 +0800

    [chore] 测试release脚本

--- build/release.js
+++ build/release.js
@@ -9,4 +9,4 @@ const version = require('../package').version;
 
 const getTags = () => execSync('git tag').toString().split('\n');
 
-!getTags().includes(version) && execSync(`git tag v${version}`);
+!getTags().includes(version) && execSync(`git tag v${version} && git push origin master:master --tags`);
